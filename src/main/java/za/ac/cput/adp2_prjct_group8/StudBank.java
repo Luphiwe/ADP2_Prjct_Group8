d4d9d819f3fdaeff19d36f2a94b22aaa9ccdc811
Luphiwe <sluphiwe@gmail.com>
2021-04-01 23:20:25 +0200

    This is the edited file with complete code.

---
+++ src/main/java/za/ac/cput/adp2_prjct_group8/StudBank.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adp2_prjct_group8;
+
+/**
+ *
+ * @author Luphiwe Sikupela
+ * 216060133
+ */
+public class StudBank {
+    
+    private String studName, id, bankName;
+
+    public String getStudName() {
+        return studName;
+    }
+
+    public void setStudName(String studName) {
+        this.studName = studName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    @Override
+    public String toString() {
+        return "StudBank{" + "studName=" + studName + ", id=" + id + ", bankName=" + bankName + '}';
+    }
+    
+    
+   
+    
+}
